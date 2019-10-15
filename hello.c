6a2c10810981a07f3340b3f7b36e9bcb3e46f754
monasinliu <31915974+monasinliu@users.noreply.github.com>
2019-10-15 08:40:44 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@ int main(void)
   CURLcode res;
   curl = curl_easy_init();
   if(curl){
-    curl_easy_setopt(curl,CURLLOPT_URL,"http://myip.ipip.net/");
+    curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
     res = curl_easy_perform(curl);
     if(res != CURLE_OK){
       fprintf(stderr,"curl_easy_perform() failed:%s\n",curl_easy_strerror(res));
